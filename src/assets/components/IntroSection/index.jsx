7e10d4488e8a9b5be5e394a20700dbcd3c093741
phatdang <phatdang19052004@gmail.com>
2025-04-18 16:25:52 +0700

    xay dung IntroSection

--- src/assets/components/IntroSection/index.jsx
+++ src/assets/components/IntroSection/index.jsx
@@ -1,9 +1,14 @@
-export default function Introsection() {
+// src/components/IntroSection.jsx
+
+export default function IntroSection() {
   return (
-    <>
-      <div className="p-10 border">
-        <p className="text-4xl underline uppercase">Introsection</p>
-      </div>
-    </>
+    <section className="max-w-xl px-6 md:px-0">
+      <h1 className="text-4xl md:text-5xl font-bold text-blue-800 leading-snug mb-4">
+        Nơi dành cho những cuộc trò chuyện có ý nghĩa
+      </h1>
+      <p className="text-lg text-gray-600">
+        Messenger giúp bạn dễ dàng và nhanh chóng trò chuyện với những người thân yêu
+      </p>
+    </section>
   );
 }
