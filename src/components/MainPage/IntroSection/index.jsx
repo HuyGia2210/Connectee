3bc60f038929855f84714953400a6c1d0986ab5f
phatdang <phatdang19052004@gmail.com>
2025-05-06 20:50:02 +0700

    them responsive cho IntroSection

--- src/components/MainPage/IntroSection/index.jsx
+++ src/components/MainPage/IntroSection/index.jsx
@@ -1,12 +1,10 @@
-// src/components/IntroSection.jsx
-
 export default function IntroSection() {
   return (
-    <section className="max-w-xl px-6 md:px-0">
-      <h1 className="text-4xl md:text-5xl font-bold text-blue-800 leading-snug mb-6 mt-4">
+    <section className="max-w-xl px-4 sm:px-6 md:px-0">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-blue-800 leading-snug mb-4 mt-4">
         Nơi dành cho những cuộc trò chuyện có ý nghĩa
       </h1>
-      <p className="text-lg text-gray-600 mb-6">
+      <p className="text-base sm:text-lg text-gray-600 mb-6">
         Connectee giúp bạn dễ dàng và nhanh chóng trò chuyện với những người thân yêu
       </p>
     </section>
