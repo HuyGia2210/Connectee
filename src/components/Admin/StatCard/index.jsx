d0caecfd5c787263ac28fcc375b1b8d7feb385ff
phatdang <phatdang19052004@gmail.com>
2025-05-06 21:57:31 +0700

    them responsive cho StatCard

--- src/components/Admin/StatCard/index.jsx
+++ src/components/Admin/StatCard/index.jsx
@@ -1,13 +1,12 @@
-// src/assets/components/StatCard/index.jsx
-export default function StatCard({ title, value, textColor  }) {
-    return (
-      <div className="bg-gray-200 p-4 rounded-lg shadow-xl ">
-        <h3 className="text-md text-gray-600">{title}</h3>
-        <div className="flex items-center justify-center mt-2">
-          <div>
-            <div className={`text-2xl font-semibold ${textColor}`}>{value}</div>
-          </div>
+export default function StatCard({ title, value, textColor }) {
+  return (
+    <div className="bg-gradient-to-r from-white to-gray-50 p-4 sm:p-6 rounded-lg shadow-xl hover:shadow-2xl transition duration-200">
+      <h3 className="text-sm sm:text-lg font-semibold text-gray-600">{title}</h3>
+      <div className="flex items-center justify-center mt-2 sm:mt-3">
+        <div>
+          <div className={`text-xl sm:text-2xl font-bold ${textColor}`}>{value}</div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
